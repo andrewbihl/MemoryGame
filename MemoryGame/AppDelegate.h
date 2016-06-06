240439d5ae98769458c888279ba896631a8e3eb9
andrewbihl <andrewbihlva@gmail.com>
2016-06-06 14:37:25 -0500

    Initial Commit

---
+++ MemoryGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MemoryGame
+//
+//  Created by Andrew Bihl on 6/6/16.
+//  Copyright © 2016 Andrew Bihl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
